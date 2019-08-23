23c5c8cbbd56dbd948e5e9f0a18f11a10818e1ea
xezbeth <sharoonalibrahim@gmail.com>
2019-08-23 18:06:54 +0530

    added model

--- model.py
+++ model.py
@@ -11,6 +11,22 @@ class build_model:
         self.model.add(CuDNNLSTM(3,batch_input_shape=[None,None,None],return_sequence = False))
         self.model.compile(loss="mse",optimizer="adam",metrices=["accuracy"])
 
-    def train(self,input,output,epochs = 10):
+    def train(self,x_train,y_train,epochs = 10):
 
-        return self.model.fit(input,output,epochs = epochs)
+        return self.model.fit(x_train,y_train,epochs = epochs)
+
+    def test(self,x_test,y_test):
+
+        return self.model.evaluate(x_test,y_test)
+
+    def predict(self,x_predict):
+
+        return self.model.predict(x_predict)
+
+    def save_model(self,path):
+
+        self.model.save_weights(path)
+
+    def load_model(self,path):
+
+        return self.model.load_weights(path)
